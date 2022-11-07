e9eebdfbc356db534c335eb8dbd8e8387036afbc
Jorge Aguilar <jorge.aguilarnc@gmail.com>
2022-11-06 21:16:35 -0500

    test

--- client/src/components/ProjectCard/index.js
+++ client/src/components/ProjectCard/index.js
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card';
 function ProjectCard(props){
     return (
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Img variant="top" src={props.image} style={{ height: '60px'}} />
           <Card.Body>
             <Card.Title>{props.title}</Card.Title>
             <Card.Text>
