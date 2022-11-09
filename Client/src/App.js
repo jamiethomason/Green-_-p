d5097e3acc95b6f2bcf026cb89d66368076e915f
Zechariah Foxx <zak.foxx@gmail.com>
2022-11-08 21:01:18 -0500

    create changes

---
+++ Client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+// import Footer from "./components/Footer";
+// import Header from "./components/Header"
+
+const App = () => <PortfolioContainer />;
+
+//     <div className="App">
+//     <Nav></Nav>
+
+//     </div>
+//   );
+// }
+
+export default App;
