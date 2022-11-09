89c38c3b134389bd3a6f7c36f5108c71b4a3d22b
Jamie Thomason <jamie.revill@gmail.com>
2022-11-08 21:31:37 -0500

    added blurb and changed header size

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -4,11 +4,18 @@ import React from 'react';
  function About() {
   return (
     <div className= "center">
-      <h1>Green Up</h1>
-
+      <h1>Green ↑_↑p</h1>
       <p>
       Green Up is a site for plant lovers.
+      <br/>
+      <br/>
+      We are committed to sharing tips, ideas, blogs and photos to create a like-minded community of those who love to grow and tend.
+      <br/>
+      <br/>
+      If you're interested in learning more or sharing your own love of plants, please visit our Login tab to join!
+      <br/>
       </p>
+      <img src="growingplant.jpg" class="rounded mx-auto d-block" alt="growingplants"></img>
     </div>
   );
 }
